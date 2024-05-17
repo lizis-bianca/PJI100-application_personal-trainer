73b86712c3aa8bd64f09572d9db15b8f9ed11ad4
lisiz <91273140+lizis-bianca@users.noreply.github.com>
2024-05-16 21:54:16 -0300

    fix(components): `Dashboard/Workouts/WorkoutCardTimer` add table workouts

--- components/Dashboard/Workouts/WorkoutCardTimer.tsx
+++ components/Dashboard/Workouts/WorkoutCardTimer.tsx
@@ -5,6 +5,8 @@ import { useGetScreenWidth } from '@/lib/hooks/useGetScreenWidth';
 import TimerRecord from './Timer/TimerRecord';
 import { Dumbbell } from 'lucide-react';
 import { DBWorkout } from '@/types/Workout';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Badge } from '@/components/ui/badge';
 
 export default function WorkoutCardTimer({ workout }: { workout: DBWorkout }) {
     const { screenWidth } = useGetScreenWidth();
@@ -29,6 +31,38 @@ export default function WorkoutCardTimer({ workout }: { workout: DBWorkout }) {
                 <h1><b>Praticando : <span style={{ color: '#8a2be2' }}>{ workout.workout.title }</span></b></h1>
                 <p>{ workout.workout.description }</p>
                 <TimerRecord/>
+            </div>
+            <br/>
+            <div className=' mb-1 min-h-[2.5rem] flex items-center overflow-hidden '>
+                <Table className='w-full h-full font-sm mt-2 overflow-hidden'>
+                    <TableHeader>
+                        <TableRow className=''>
+                            <TableHead className='text-xs'>Nome</TableHead>
+                            <TableHead className='text-xs'>Séries</TableHead>
+                            <TableHead className='text-xs'>Nível</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody className='relative'>
+                        <div className='absolute w-full h-full bg-gradient-to-b from-transparent to-card z-[1]'></div>
+                        {workout.workout.exercises.map((exercise, index) => (
+                            <>
+                                <TableRow key={index}>
+                                    <TableCell className='text-xs'>{exercise.name}</TableCell>
+                                    <TableCell className='text-xs'>
+                                        <Badge variant={"secondary"}>
+                                            {exercise.muscles[0]}
+                                        </Badge>
+                                    </TableCell>
+                                    <TableCell className='text-xs'>
+                                        <Badge variant={exercise.level[0] as any}>
+                                            {exercise.level[0]}
+                                        </Badge>
+                                    </TableCell>
+                                </TableRow>
+                            </>
+                        ))}
+                    </TableBody>
+                </Table>
             </div>
         </DrawerContent>
     </Drawer>
